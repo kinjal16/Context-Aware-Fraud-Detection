4b86c2e27075426a42cdb9d5b92acd85e2d1cdad
Laxmi <laxmiphalak@gmail.com>
2015-11-28 15:21:08 -0800

    # This is a combination of 2 commits. # The first commit's message is:
    Express 4.0 changes
     - Change paths in all files for css, js and images
     - Add packages in package.json for redis, request and express-session modules
     - Add session secret code in app.js
    
    # This is the 2nd commit message:
    
    sidebar

--- app.js
+++ app.js
@@ -9,19 +9,23 @@ var express = require("express");
 var path = require("path");
 var	ejs = require("ejs");
 var request = require("request");
-var client = require("./redis-client.js");
+var client = require("./routes/redis-client.js");
 var app = express();
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var bodyParser=require('body-parser');
 var multer = require('multer');
 var methodOverride = require('method-override');
+var session = require('express-session');
 var errorhandler = require('errorhandler');
 var app = module.exports = express();
 
 app.set('port', '3000');
 app.use(logger('dev'));
 app.use(methodOverride());
+app.use(session({ resave: true,
+    saveUninitialized: true,
+    secret: 'uwotm8' }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('views', path.join(__dirname, 'views'));
@@ -42,22 +46,3 @@ var server = http.createServer(app);
 server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
-
-// all environments
-/*app.configure(function () {
-	app.use(express.cookieParser());
-	app.use(express.session({secret: "LP1988"}));
-	app.use(express.bodyParser());
-	app.use(express.json());
-	app.use(express.methodOverride());
-	app.use(app.router);
-	app.use(express.static(path.join(application_root, "")));
-	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});*/
-
-//require('./routes/router')(app);
-/*
->>>>>>> 2a7b267a5e6c78d710f1a3073c3202c81bc24ae4
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});*/
